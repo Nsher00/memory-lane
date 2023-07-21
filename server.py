f2e35f51b1555c3b341cdd403a63e21f0324f503
Nsher00 <jimmysherwood9@gmail.com>
2023-07-20 23:43:34 -0600

    added the login page with working login form.

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from jinja2 import StrictUndefined
+from forms import LoginForm
+from json import JSONEncoder
 
 app = Flask(__name__)
 app.secret_key = 'superdupersecret'
@@ -8,6 +10,11 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', login_form = login_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
